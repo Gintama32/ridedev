015dbc97f09ccd3bbb40b9efd63ffbd629b92939
Ginatam32 <fluffey6@gmail.com>
2025-02-21 22:53:26 -0600

    fixed PostRide

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,72 +4,72 @@ import {Link} from 'react-router-dom';
 function Form() {
   return (
     <div>
-    <article className="br3 shadow-5 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
+    <article className="br3 shadow-5 ba b--white -10 mv4 w-100 w-50-m w-25-l mw6 center">
       <main className="pa4 green-80">
         <div className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f1 fw6 ph0 mh0 center black">Ride Info</legend>
+            <legend className="f1 fw6 ph0 mh0 center white">Ride Info</legend>
             <div className="mt3" >
-              <label className="db fw6 lh-copy f6 black" htmlFor="destination-address">
+              <label className="db fw6 lh-copy f6 white" htmlFor="destination-address">
                 Destination Address:
               </label>
               <input
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                className="pa2 input-reset ba b--white bg-transparent hover-bg-navy hover-white w-100"
                 type="text"
                 name="destination-address"
                 id="destination-address"
               />
             </div>
             <div className="mt3" >
-              <label className="db fw6 lh-copy f6 black" htmlFor="arriving-time">
+              <label className="db fw6 lh-copy f6 white" htmlFor="arriving-time">
                 Arriving Time To Destination:
               </label>
               <input
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                className="pa2 input-reset ba b--white bg-transparent hover-bg-navy hover-white w-100"
                 type="time"
                 name="arriving-time"
                 id="arriving-time"
               />
             </div>
             <div className="mt3" >
-              <label className="db fw6 lh-copy f6 black" htmlFor="date">
+              <label className="db fw6 lh-copy f6 white" htmlFor="date">
                 Date:
               </label>
               <input
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                className="pa2 input-reset ba b--white bg-transparent hover-bg-navy hover-white w-100"
                 type="date"
                 name="date"
                 id="date"
               />
             </div>
             <div className="mt3" >
-              <label className="db fw6 lh-copy f6 black" htmlFor="pickup-location">
+              <label className="db fw6 lh-copy f6 white" htmlFor="pickup-location">
                 Pickup Loction:
               </label>
               <input
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                className="pa2 input-reset ba b--white bg-transparent hover-bg-navy hover-white w-100"
                 type="text"
                 name="pickup-location"
                 id="pickup-location"
               />
             </div>
             <div className="mt3" >
-              <label className="db fw6 lh-copy f6 black" htmlFor="pickup-time">
+              <label className="db fw6 lh-copy f6 white" htmlFor="pickup-time">
                 Pickup Time:
               </label>
               <input
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                className="pa2 input-reset ba b--white bg-transparent hover-bg-navy hover-white w-100"
                 type="time"
                 name="pickup-time"
                 id="pickup-time"
               />
             </div>
             <div className="mt3" >
-              <label className="db fw6 lh-copy f6 black" htmlFor="number_of_passengers">
+              <label className="db fw6 lh-copy f6 white" htmlFor="number_of_passengers">
                 Number of Passengers:
               </label>
               <input
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                className="pa2 input-reset ba b--white bg-transparent hover-bg-navy hover-white w-100"
                 type="number"
                 name="number_of_passengers"
                 id="number_of_passengers"
--- src/pages/postRide/PostRide.jsx
+++ src/pages/postRide/PostRide.jsx
@@ -7,11 +7,9 @@ function PostRide() {
       <div className="overlay"></div>
         <video src = {bg} autoPlay loop muted/>
         <div className="content">
-      <h1 className="center gold">Post your Ride</h1>
-      <div className="center list-container">
-        <Form/>
-      </div>
-      </div>
+          <h1 className="center white mt5">Post your Ride</h1>
+          <Form/>
+        </div>
     </div>
   );
 }
